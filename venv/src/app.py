67d4c192088571cdec31579183bd18ddb2993263
stew0734 <stew0734@umn.edu>
2025-10-16 00:15:20 -0500

    added front-end apiPost/Get and connected front-end and back-end methods

--- venv/src/app.py
+++ venv/src/app.py
@@ -5,7 +5,6 @@ from flask import Flask, redirect, request, jsonify, url_for, render_template, R
 from server_api import ServerAPI
 from logger import request_logger
 
-
 app = Flask(__name__)
 
 def expect_json(keys) :
@@ -44,53 +43,70 @@ def server_api_route(route, methods=['GET']) :
         return wrapper
     return decorator
 
-
 def check_response(res) :
     if res['status'] != 0:
-        return {"error": res['message']}, 500
-    return res['data']
+        return format_response(1, res['message'], res['data'])
+    return format_response(0, res['message'], res['data'])
+    
+
+def format_response(status, message, data):
+    status_out = False
+    data_key = "error"
+    if status == 0:
+        data_key = "data"
+        status_out = True
+    
+    return {
+        "status" : status_out,
+        "message" : message,
+        data_key : data
+    }
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 #VM dashboard
-@app.route('/vms')
+@app.route('/vm')
 def vm_dashboard() :
-    return render_template('vms.html')
+    return render_template('vm.html')
 
-@server_api_route('/vms/list', methods=['GET'])
+@server_api_route('/vm/list', methods=['GET'])
 def list_vms() :
     res = ServerAPI.list_vms()
     return check_response(res)
 
-@server_api_route('/vms/<name>', methods=['GET'])
+@server_api_route('/vm/<name>', methods=['GET'])
 def vm_info(name) :
     res = ServerAPI.vm_info(name)
     return check_response(res)
 
-@server_api_route('/vms/<name>/state', methods=['GET'])
+@server_api_route('/vm/<name>/state', methods=['GET'])
 def vm_state(name) :
     res = ServerAPI.vm_state(name)
     return check_response(res)
 
-@server_api_route('/vms/<name>/start', methods=['POST'])
+@server_api_route('/vm/<name>/start', methods=['POST'])
 def start_vm(name) :
     res = ServerAPI.start_vm(name)
     return check_response(res)
 
-@server_api_route('/vms/<name>/stop', methods=['POST'])
+@server_api_route('/vm/<name>/stop', methods=['POST'])
 def stop_vm(name) :
     res = ServerAPI.stop_vm(name)
     return check_response(res)
 
-@server_api_route('/vms/<name>/reboot', methods=['POST'])
+@server_api_route('/vm/<name>/reboot', methods=['POST'])
 def reboot_vm(name) :
     res = ServerAPI.reboot_vm(name)
     return check_response(res)
 
+@server_api_route('/vm/create', methods=['POST'])
+@expect_json(["name", "memory", "vcpus", "os", "network", "disk_path", "iso"])
+def create_vm(data) :
+    res = ServerAPI.create_vm(data)
+    return check_response(res)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
 
--- venv/src/server_api.py
+++ venv/src/server_api.py
@@ -5,7 +5,7 @@ class ServerAPI:
     @staticmethod
     def response(status_code, message, data=None):
         return {
-            "status_code": status_code,
+            "status": status_code,
             "message": message,
             "data": data
         }
@@ -15,24 +15,27 @@ class ServerAPI:
         """
         retuns dictionary with vm name keys and vm {id : , name : , state : } as value
         """
-        result = {}
-        cmd, success = run_command('virsh list --all')
+        payload = {}
+        result, success = run_command('virsh list --all')
         if not success:
-            return ServerAPI.response(1, "Failed to list VMs", cmd)
-    
-        lines = cmd.stdout.strip().split('\n')[2:]
+            return ServerAPI.response(1, "Failed to list VMs", result)
+
+        lines = result.stdout.strip().split('\n')[2:]
         for line in lines :
             parts = line.split(None, 2)
             if (len(parts) == 3) :
                 vm_id, name, state = parts
-                result[name] = {'id' : vm_id, 'name' : name, 'state' : state}   
-        return ServerAPI.response(0, "list of vms", result)
-    
+            payload[name] = {'id' : vm_id, 'name' : name, 'state' : state}   
+        return ServerAPI.response(0, "list of vms", payload)
+
     @staticmethod
     def isVM(name) :
         vms = ServerAPI.list_vms()
-        return name in vms
-    
+        if vms["status"] != 0 :
+            return False
+        # return name in    
+        return name in vms["data"].keys()
+
     @staticmethod
     def vm_state(name) :
         """
@@ -51,11 +54,12 @@ class ServerAPI:
         retuns output from starting a vm
         """
         if (not ServerAPI.isVM(name)) :
+            print(name)
             return ServerAPI.response(1, "invalid vm name", None)
         result, success = run_command(['virsh', 'start', name])
         if not success:
             return ServerAPI.response(1, "Failed to start VM", result)
-        return ServerAPI.response(0, "VM started", result.stdout.strip())
+        return ServerAPI.response(0, f'started {name}', result.stdout.strip())
 
 
     @staticmethod
@@ -68,7 +72,7 @@ class ServerAPI:
         result, success = run_command(f'virsh reboot {name}')
         if not success:
             return ServerAPI.response(1, "Failed to reboot VM", result)
-        return ServerAPI.response(0, "VM rebooted", result.stdout.strip())
+        return ServerAPI.response(0, f'rebooted {name}', result.stdout.strip())
 
     @staticmethod
     def stop_vm(name) :
@@ -81,14 +85,37 @@ class ServerAPI:
 
         result, success = run_command(f'virsh shutdown {name}')
         if not success:
-            return ServerAPI.response(1, "Failed to stop VM", result)
+            return ServerAPI.response(1, "Failed to stop VM: run_command utility failed", result)
             # Wait for the VM to shut off
         for _ in range(10) :
-            state = run_command(f'virsh domstate {name}').stdout.strip()
+            state = ServerAPI.vm_state(name)["data"]
             if state == "shut off" :
-                return ServerAPI.response(0, "VM stopped", result.stdout.strip())
+                return ServerAPI.response(0, f'stopped {name}', result.stdout.strip())
             time.sleep(1)
-        return ServerAPI.response(1, "Failed to stop VM: Timeout", None)
+        return ServerAPI.response(1, "Failed to stop VM: Timeout", result.stdout.strip())
+
+    @staticmethod
+    def create_vm(data) :
+        """
+        creates a vm from an xml file
+        """
+        result, success = run_command(f'echo {data["name"]} {data["memory"]} {data["vcpus"]} {data["os"]} {data["network"]} {data["disk_path"]} {data["iso"]}')
+        if not success:
+            return ServerAPI.response(1, "Failed to create VM", result)
+        return ServerAPI.response(0, f'created {data["name"]}', result.stdout.strip())
+
+    @staticmethod
+    def run_cmd(cmd, timeout=30):
+        """
+        runs a command and returns (result, success)
+        result is subprocess.CompletedProcess on success
+        result is error dictionary on failure
+        success is boolean
+        """
+        result, success = run_command(cmd, timeout)
+        if not success:
+            return ServerAPI.response(1, "Command failed", result)
+        return ServerAPI.response(0, "Command succeeded", result.stdout.strip())
 
 def run_command(cmd, timeout=30):
         try:
